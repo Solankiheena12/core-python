38c6c78904a663fd545c41b30b9519d49036358e
Solankiheena12 <147366262+Solankiheena12@users.noreply.github.com>
2023-12-05 20:06:31 -0800

    Arithmetic Operator

---
+++ operators.py
@@ -0,0 +1,100 @@
+# # Arithmetical Operator:
+
+# # Addition :
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# x = 10.5
+# y = 11.3
+# z = x + y
+# print(z)
+
+
+# # Subtraction :
+# a = 10
+# b = 5
+# c = a - b
+# print(c)
+
+# x = 20.35
+# y = 1.9
+# z = x - y
+# print(z)
+
+
+# # Multiplication :
+# a = 10
+# b = 5
+# c = a * b
+# print(c)
+
+# x = 10.5
+# y = 11.3
+# z = x * y
+# print(z)
+
+
+# # Division :
+# a = 10
+# b = 5
+# c = a / b
+# print(c)
+
+# x = 150
+# y = 10
+# z = x / y
+# print(z)
+
+# # Modulus :
+# a = 225              
+# b = 20
+# c = a % b
+# print(c)
+
+# x = 150
+# y = 9
+# z = x % y
+# print(z)
+
+# # Exponentiation :
+# a = 10
+# b = 5
+# c = a ** b
+# print(c)
+
+# x = 4
+# y = 4
+# z = x ** y
+# print(z)
+
+# a = 2
+# b = 12
+# c = a ** b
+# print(c)
+
+# # Floor division :
+# a = 10
+# b = 5
+# c = a // b
+# print(c)
+
+# x = 150
+# y = 10
+# z = x // y
+# print(z)
+
+# x = 104
+# y = 10
+# z = x // y
+# print(z)
+
+
+
+
+
+
+
+
+# Assignment Operators :
